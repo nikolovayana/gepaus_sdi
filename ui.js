d30686be3b5b9f834c9280cc5af975a5981839c2
Rosen Ruev <rosenruev@gmail.com>
2025-01-21 09:57:01 +0100

    partially fixed the navbar bug

--- ui.js
+++ ui.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('waiting')
-    console.log(window.innerHeight)
     if (window.innerHeight < 615) {
+        document.querySelector(".navbar").style.display = "none";
         document.querySelector(".arrow").addEventListener("mouseover", () => {
             console.log("success")
             document.querySelector(".navbar").style.display = "flex";
@@ -11,4 +10,22 @@ document.addEventListener("DOMContentLoaded", () => {
             document.querySelector(".navbar").style.display = "none";
         })
     }
+    window.addEventListener('resize', (event) => {
+        console.log(event.type)
+        if (window.innerHeight < 615) {
+            document.querySelector(".navbar").style.display = "none";
+            document.querySelector(".arrow").addEventListener("mouseover", () => {
+                console.log("success")
+                document.querySelector(".navbar").style.display = "flex";
+            })
+            document.querySelector(".navbar").addEventListener("mouseout", () => {
+                console.log("success")
+                document.querySelector(".navbar").style.display = "none";
+            })
+        }
+        else {
+            document.querySelector(".navbar").style.display = "flex";
+        }
+
+    })
     })
