d54ddfbd229c66359a3eec5c35e4aa304f5025a8
Rosen Ruev <rosenruev@gmail.com>
2025-02-06 15:05:36 +0100

    changed the mobile display

--- ui.js
+++ ui.js
@@ -1,38 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navbar = document.querySelector(".navbar");
     const media = document.querySelector(".media-container");
-    if (window.innerHeight < 615) {
-        toggleNavbar();
-    }
-    window.addEventListener('resize', () => {
-        if (window.innerHeight < 615) {
-            navbar.style.opacity = "0";
-            toggleNavbar();
-        }
-        else {
-            navbar.style.opacity = "1";
-        }
-
-        if (screen.width < 980) {
-            media.classList.add("hidden");
-        }
-    })
+    const doughnut = document.querySelector(".doughnut-container");
+    const bar = document.querySelector(".bar-container");
 
     if (window.innerWidth < 980) {
         media.classList.add("hidden");
     }
 
-    document.querySelector(".logo").addEventListener("click", () => {
-        media.classList.toggle("hidden");
+    if (window.innerWidth < 540) {
+        bar.classList.add("displayed");
+        console.log("success");
+    }
 
+    window.addEventListener('resize', () => {
+        if (screen.width < 980) {
+            media.classList.add("hidden");
+        }
+
+        if (window.innerWidth < 540) {
+            bar.classList.add("displayed");
+            console.log("success")
+        }
+
+        if (window.innerWidth > 540) {
+            bar.classList.remove("displayed");
+            doughnut.classList.remove("displayed");
+        }
     })
 
-    function toggleNavbar() {
-        navbar.addEventListener("mouseenter", () => {
-            document.querySelector(".navbar").style.opacity = "1";
-        })
-        navbar.addEventListener("mouseleave", () => {
-            navbar.style.opacity = "0";          
-        })
-    }
+    document.querySelector(".logo").addEventListener("click", () => {
+        media.classList.toggle("hidden");
+    })
+
+    document.querySelector(".toggle").addEventListener("click", () => {
+        doughnut.classList.toggle("displayed");
+        bar.classList.toggle("displayed");
+    })
 })
